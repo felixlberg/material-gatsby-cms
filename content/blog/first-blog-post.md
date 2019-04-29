00dd2ce11e6b92f821b50e5703ba9372a4f6e503
Felix <felix.lberg@gmail.com>
2019-04-30 00:22:26 +0200

    added firs blug functions

---
+++ content/blog/first-blog-post.md
@@ -0,0 +1,5 @@
+---
+path: "/blog/first-blog-post"
+date: "2017-11-07"
+title: "My first blog post"
+---
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "blog",
+      path: `${__dirname}/content/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    "gatsby-plugin-netlify-cms",
+  ]
+
+// End of module.exports
 }
